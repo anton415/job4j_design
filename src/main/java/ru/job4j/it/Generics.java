0948c6bdb778cad728df21061d1da91cf279f359
Anton Serdyuchenko <anton415@gmail.com>
2021-11-14 19:44:44 +0300

    0. Что такое обобщенные типы (generics) [#4952]

---
+++ src/main/java/ru/job4j/it/Generics.java
@@ -0,0 +1,55 @@
+package ru.job4j.it;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Generics {
+    public static void main(String[] args) {
+        Generics gen = new Generics();
+        List<Animal> first = new ArrayList<>();
+        List<Predator> second = new ArrayList<>();
+        List<Tiger> third = new ArrayList<>();
+        first.add(new Animal());
+        second.add(new Predator());
+        third.add(new Tiger());
+
+        gen.printObject(first);
+        gen.printObject(second);
+        gen.printObject(third);
+        System.out.println();
+
+//        gen.printBoundedWildCard(first);
+        gen.printBoundedWildCard(second);
+        gen.printBoundedWildCard(third);
+        System.out.println();
+
+        gen.printLowerBoundedWildCard(first);
+        gen.printLowerBoundedWildCard(second);
+//        gen.printLowerBoundedWildCard(third);
+    }
+
+    // Работает без ограничений, т.е. в него можно передавать коллекцию, которая хранит любые типы.
+    public void printObject(List<?> list) {
+        for (Iterator<?> it = list.iterator(); it.hasNext();) {
+            Object next = it.next();
+            System.out.println("Текущий элемент: " + next);
+        }
+    }
+
+    // должен иметь ограничение сверху и ограничиваться классом Predator.
+    public void printBoundedWildCard(List<? extends Predator> list) {
+        for (Iterator<? extends Predator> it = list.iterator(); it.hasNext();) {
+            Object next = it.next();
+            System.out.println("Текущий элемент: " + next);
+        }
+    }
+
+    // должен иметь ограничение снизу и ограничиваться классом Predator.
+    public void printLowerBoundedWildCard(List<? super Predator> list) {
+        for (Iterator<? super Predator> it = list.iterator(); it.hasNext();) {
+            Object next = it.next();
+            System.out.println("Текущий элемент: " + next);
+        }
+    }
+}
---
+++ src/main/java/ru/job4j/it/Predator.java
@@ -0,0 +1,4 @@
+package ru.job4j.it;
+
+public class Predator extends Animal {
+}
