9b2fca614a9b55d548a83c8cb253d512244722c5
Anton Serdyuchenko <anton415@gmail.com>
2022-01-13 09:30:37 +0300

    0.2. FileInputStream [#4898 #250213]

--- src/main/java/ru/job4j/io/EvenNumberFile.java
+++ src/main/java/ru/job4j/io/EvenNumberFile.java
@@ -8,11 +8,14 @@ public class EvenNumberFile {
             StringBuilder text = new StringBuilder();
             int read;
             while ((read = in.read()) != -1) {
-                if (read % 2 == 0) {
-                    text.append((char) read);
+                text.append((char) read);
+            }
+            String[] lines = text.toString().split(System.lineSeparator());
+            for (String line : lines) {
+                if (Integer.parseInt(line) % 2 == 0) {
+                    System.out.println(line);
                 }
             }
-            System.out.println(text);
         } catch (Exception e) {
             e.printStackTrace();
         }
