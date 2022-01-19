46fc3a463f0278a56f34d790bdee6ef21f66be01
Anton Serdyuchenko <anton415@gmail.com>
2022-01-19 15:10:28 +0300

    4.0. File [#252491]

---
+++ src/main/java/ru/job4j/io/Dir.java
@@ -0,0 +1,14 @@
+package ru.job4j.io;
+
+import java.io.File;
+
+public class Dir {
+    public static void main(String[] args) {
+        File file = new File("log.txt");
+        if (!file.exists()) {
+            throw new IllegalArgumentException(String.format("Not exist %s", file.getAbsoluteFile()));
+        }
+        System.out.println(file.getName());
+        System.out.println(file.length());
+    }
+}
