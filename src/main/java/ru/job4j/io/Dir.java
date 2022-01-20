86be765ec88acbc6a02eb34752b76602988f935a
Anton Serdyuchenko <anton415@gmail.com>
2022-01-20 16:53:38 +0300

    4.0. File [#252491 #252957]

--- src/main/java/ru/job4j/io/Dir.java
+++ src/main/java/ru/job4j/io/Dir.java
@@ -4,11 +4,15 @@ import java.io.File;
 
 public class Dir {
     public static void main(String[] args) {
-        File file = new File("data/log.txt");
+        File file = new File("data");
         if (!file.exists()) {
             throw new IllegalArgumentException(String.format("Not exist %s", file.getAbsoluteFile()));
         }
-        System.out.println(file.getName());
-        System.out.println(file.length());
+        if (!file.isDirectory()) {
+            throw new IllegalArgumentException(String.format("Not directory %s", file.getAbsoluteFile()));
+        }
+        for (File subfile : file.listFiles()) {
+            System.out.println(String.format("name: %s (size: %s)", subfile.getName(), subfile.length()));
+        }
     }
 }
