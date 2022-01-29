2a41d4c40e8065b6d032027cbe0c535bcc28e9ef
Anton Serdyuchenko <anton415@gmail.com>
2022-01-30 00:43:22 +0300

    5. Валидация параметров запуска. [#246865 #257652]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -9,8 +9,13 @@ import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        if (args.length == 0) {
-            throw new IllegalArgumentException("Path is null. You need to add argument for path.");
+        if (args.length != 2) {
+            throw new IllegalArgumentException(
+                    "Incorrect number of arguments."
+                    + " You need to add two arguments."
+                    + "First - path. Second - file type."
+                    + "Example: \". xml\"."
+            );
         }
         Path start = Paths.get(args[0]);
         if (!start.toFile().exists()) {
@@ -18,9 +23,6 @@ public class Search {
         }
         if (!start.toFile().isDirectory()) {
             throw new IllegalArgumentException(String.format("Not directory %s", start.toAbsolutePath()));
-        }
-        if (args.length != 2) {
-            throw new IllegalArgumentException("Incorrect number of arguments. You need to add two arguments.");
         }
         search(start, p -> p.toFile().getName().endsWith(args[1])).forEach(System.out::println);
     }
