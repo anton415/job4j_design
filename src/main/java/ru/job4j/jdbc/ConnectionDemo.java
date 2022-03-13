032fb9d61b7db70432d12cec1c04083dcf216a6a
Anton Serdyuchenko <anton415@gmail.com>
2022-03-13 15:51:25 +0300

    0. JDBC [#6863]
    - Получение настроек из файла
    - Подключение к БД
    - Вывод лога

---
+++ src/main/java/ru/job4j/jdbc/ConnectionDemo.java
@@ -0,0 +1,30 @@
+package ru.job4j.jdbc;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ru.job4j.io.Config;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionDemo {
+    private static final Logger LOG = LoggerFactory.getLogger(ConnectionDemo.class.getName());
+
+    public static void main(String[] args) throws SQLException {
+        String path = "data/app.properties";
+        Config config = new Config(path);
+        config.load();
+
+        String url = config.value("postgres.url");
+        String login = config.value("postgres.login");
+        String password = config.value("postgres.password");
+
+        try (Connection connection = DriverManager.getConnection(url, login, password)) {
+            DatabaseMetaData metaData = connection.getMetaData();
+            LOG.info(metaData.getUserName());
+            LOG.info(metaData.getURL());
+        }
+    }
+}
