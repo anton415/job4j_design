0d81eadda8482b3d7935b50aba445bb205b12820
Anton Serdyuchenko <anton415@gmail.com>
2022-03-16 20:58:17 +0300

    0. JDBC [#6863 #274730]
    - загрузка драйвера.

--- src/main/java/ru/job4j/jdbc/ConnectionDemo.java
+++ src/main/java/ru/job4j/jdbc/ConnectionDemo.java
@@ -12,11 +12,12 @@ import java.sql.SQLException;
 public class ConnectionDemo {
     private static final Logger LOG = LoggerFactory.getLogger(ConnectionDemo.class.getName());
 
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
         String path = "data/app.properties";
         Config config = new Config(path);
         config.load();
 
+        Class.forName(config.value("postgres.driver"));
         String url = config.value("postgres.url");
         String login = config.value("postgres.login");
         String password = config.value("postgres.password");
